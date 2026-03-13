3ae9f696312fc50711033b0c9d0911026a9f5d14
Jeremiah Webb <webmans2jeremiah@gmail.com>
2026-03-13 18:26:00 +1100

    Initial Commit

---
+++ Meshnet/MeshnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshnetApp.swift
+//  Meshnet
+//
+//  Created by Jeremiah Webb on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MeshnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
